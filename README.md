d2eae9d81d352a6b8cc38b2d02e20dacbe5928fc
GiuseppeF11 <faillagiuseppe13@gmail.com>
2023-12-01 17:01:55 +0100

    Completato esercizio ed aggiunti dettagli grafici

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# html-css-template
+# html-css-js-template
+
+js-biglietto-treno
+
+Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+STEP
+
+1) Chiedere all'utente il numero di km da percorrere
+2) Chiedere all'utente l'età
+3) Fissare il prezzo del biglietto a 0.21€ al km
+4) 
+    SE età <= 18 allora applica il 20% di sconto
+    INVECE SE età > 65 allora applica il 40% di sconto
+    ALTRIMENTI tariffa piena
+
+5) Mostra il prezzo
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,53 @@
 // Test funzionamento
 
-console.log('Template html css js')
+console.log('Template html css js');
+
+/* const errNumber = prompt('Non sono ammesse cifre') */
+
+const eta = prompt('Inserisci la tua età');
+console.log('età ' + eta +' '+ typeof eta);
+
+if (!isNaN(eta)) {
+    const nKm = prompt('Inserisci i km da che vuoi percorrere');
+    console.log('Numero Km ' + nKm +' '+ typeof nKm);
+
+    if (!isNaN(nKm)) {
+        const priceBasic = (nKm * 0.21);
+        console.log('Tariffa base: ' + priceBasic + '€')
+
+        const priceSald20 = ( priceBasic - ((priceBasic * 20) / 100)).toFixed(2)
+        console.log('Prezzo scontato al 20: ' + priceSald20 + '€' )
+
+        const priceSald40 = ( priceBasic - ((priceBasic * 40) / 100)).toFixed(2)
+        console.log('Prezzo scontato al 40: ' + priceSald40 + '€' )
+
+        if (eta < 18 ) {
+            document.getElementById('priceSald20').innerHTML = 'Prezzo col 20%: ' + priceSald20 + '€'
+        }
+
+        else if (eta > 65) {
+            document.getElementById('priceSald40').innerHTML = 'Prezzo col 40%: ' + priceSald40 + '€'
+        }
+        
+        else { 
+            document.getElementById('priceBasic').innerHTML = 'Prezzo base: ' + priceBasic + '€'
+        }
+    }
+
+    else {
+        const errLetter = prompt('Non sono ammesse lettere');
+    }
+
+}
+
+else {
+    const errLetter = prompt('Non sono ammesse lettere');
+}
+
+
+
+
+
+
+
+
